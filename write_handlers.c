c73500ffcb511862c526efe20b91fe380b2b13f9
eyramatta <135836949+eyramatta@users.noreply.github.com>
2023-10-14 16:51:22 +0000

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -1,30 +1,30 @@
-#include "main.h"
-
-/************************* WRITE HANDLE *************************/
-/**
- * handle_write_char - Prints a string
- * @c: char types.
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
- * @width: get width.
- * @precision: precision specifier
- * @size: Size specifier
- *
- * Return: Number of chars printed.
- */
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size)
-{ /* char is stored at left and paddind at buffer's right */
-	int i = 0;
-	char padd = ' ';
-
-	UNUSED(precision);
-	UNUSED(size);
-
-	if (flags & F_ZERO)
-		padd = '0';
-
-	buffer[i++] = c;
+	#include "main.h"
+	
+	/************************* WRITE HANDLE *************************/
+	/**
+	 * handle_write_char - Prints a string
+	 * @c: char types.
+	 * @buffer: Buffer array to handle print
+	 * @flags:  Calculates active flags.
+	 * @width: get width.
+	 * @precision: precision specifier
+	 * @size: Size specifier
+	 *
+	 * Return: Number of chars printed.
+	 */
+	int handle_write_char(char c, char buffer[],
+		int flags, int width, int precision, int size)
+	{ /* char is stored at left and paddind at buffer's right */
+		int i = 0;
+		char padd = ' ';
+	
+		UNUSED(precision);
+		UNUSED(size);
+	
+		if (flags & F_ZERO)
+			padd = '0';
+	
+		buffer[i++] = c;
 	buffer[i] = '\0';
 
 	if (width > 1)
